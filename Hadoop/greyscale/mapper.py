e38db3f7bea16c4909e2e45d3fc755ac1206ff89
AFFogarty <andrew.f.fogarty@gmail.com>
2015-10-09 17:38:15 -0400

    New: Python3

--- Hadoop/greyscale/mapper.py
+++ Hadoop/greyscale/mapper.py
@@ -14,4 +14,4 @@ for line in reader:
 
     luminance = (0.2126 * pixel[0]) + (0.7152 * pixel[1]) + (0.0722 * pixel[2])
     # Output index and luminance
-    print "{0}\t{1}\t{2}".format(index[0], index[1], int(math.floor(luminance)))
+    print("{0}\t{1}\t{2}".format(index[0], index[1], int(math.floor(luminance))))
