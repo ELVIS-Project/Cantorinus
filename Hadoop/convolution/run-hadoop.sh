173f7323cff773d134b21af44413943ebbf0f8de
AFFogarty <andrew.f.fogarty@gmail.com>
2015-10-14 14:29:12 -0400

    New: Direct paths to mapper and reducer

--- Hadoop/convolution/run-hadoop.sh
+++ Hadoop/convolution/run-hadoop.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 hadoop jar $HADOOP_STREAM \
- -file "mapper.py" -mapper mapper.py \
-  -file "reducer.py" -reducer reducer.py \
+ -file "/home/afogarty/Cantorinus/Hadoop/convolution/mapper.py" -mapper /home/afogarty/Cantorinus/Hadoop/convolution/mapper.py \
+  -file "/home/afogarty/Cantorinus/Hadoop/convolution/reducer.py" -reducer /home/afogarty/Cantorinus/Hadoop/convolution/reducer.py \
    -input mandrill.csv -output convolution
