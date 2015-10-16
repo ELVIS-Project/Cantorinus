536172b6cbe4ddfc682f6d34be54eda6637941aa
AFFogarty <andrew.f.fogarty@gmail.com>
2015-10-16 14:51:50 -0400

    Fixed: Comment explaining the shell command

--- Hadoop/convolution/run-hadoop.sh
+++ Hadoop/convolution/run-hadoop.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# This is the command to run the MapReduce job on Guillimin
+
 hadoop jar $HADOOP_STREAM \
  -file "/home/afogarty/Cantorinus/Hadoop/convolution/mapper.py" -mapper /home/afogarty/Cantorinus/Hadoop/convolution/mapper.py \
   -file "/home/afogarty/Cantorinus/Hadoop/convolution/reducer.py" -reducer /home/afogarty/Cantorinus/Hadoop/convolution/reducer.py \
