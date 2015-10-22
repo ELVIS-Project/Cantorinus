acdff44cb7de259b32f198cc973f6b782414392d
Marina Borsodi-Benson <marinaborsodibenson@gmail.com>
2015-10-22 13:06:37 -0400

    Fixed: unit test updated to function changes

--- Rules/mode_finder_test.py
+++ Rules/mode_finder_test.py
@@ -17,6 +17,10 @@ class ModeFinderTest(unittest.TestCase):
         the_notes = noterest.NoteRestIndexer(the_score).run()
         global part_notes
         part_notes = the_notes['noterest.NoteRestIndexer']['0'].tolist()
+        global notes
+        notes = []
+        for note in ['D4', 'E4', 'F4', 'G4', 'A4', 'B4', 'C5', 'D5', 'E5']:
+            notes.append(music21.note.Note(note))
 
     def test_finalis(self):
         """
@@ -45,7 +49,7 @@ class ModeFinderTest(unittest.TestCase):
         test_range_type() checks if range_type() returns 'complete'
         """
 
-        self.assertEqual(range_type(part_notes, p_range, fin), 'authentic')
+        self.assertEqual(range_type(p_range), 'complete')
 
     def test_species(self):
         """
@@ -55,12 +59,16 @@ class ModeFinderTest(unittest.TestCase):
 
         sp = ['T', 'S', 'T', 'T'], ['T', 'S', 'T']
         d4 = music21.note.Note('D4')
-        r_type = 'authentic'
-        self.assertEqual(species(part_notes, d4, r_type), sp)
+        r_type = 'complete'
+        self.assertEqual(species(notes, d4, r_type), sp)
+
+    def test_spec_prep(self):
         """
         test_spec_prep() checks if spec_prep() returns the sorted range,
         ['D4', 'E4', 'F4', 'G4', 'A4', 'B4', 'C5', 'D5', 'E5']
         """
+
+        self.assertEqual(spec_prep(part_notes), notes)
 
     def test_characteristic(self):
         """
